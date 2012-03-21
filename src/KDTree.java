bc8212dd4f522695b444b8d39a68c41f6fe0da43
Mark Thorhauge <markthorh@gmail.com>
2012-03-21 19:02:36 +0100

    Worked on KDTree

--- src/KDTree.java
+++ src/KDTree.java
@@ -1,32 +1,92 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class KDTree
 {
 	//Dimensions
 	private int k;
+	private KDNode root;
 
-	public KDTree(int k, Node[] nodes)
+	public KDTree(int k)
 	{
 		this.k = k;
-		build(nodes);
 	}
 	
-	private void build(Node[] nodes)
+	public void build(ArrayList<Node> nodes)
 	{
-		
+		root = new KDNode(nodes, 0);
 	}
 	
-	private void sort(Node[] nodes)
-	{
-		Arrays.sort(nodes);
-	}
-	
-	
-	
+	//Nested class
 	private class KDNode
 	{
-		KDNode right, left;
-		Node node;
+		private KDNode right, left;
+		private Node node;
+		
+		private KDNode(Node n)
+		{
+			node = n;
+		}
+		
+		private KDNode(ArrayList<Node> nodes, int depth)
+		{
+			node = expand(nodes, depth).getNode();
+		}
+		
+		public Node getNode()
+		{
+			return node;
+		}
+		
+		public KDNode expand(ArrayList<Node> nodes, int depth)
+		{
+			if (nodes.size() > 2)
+			{
+				int dimension = depth%k;
+				Node medianNode = median(nodes, depth);
+				KDNode result = new KDNode(medianNode);
+				nodes.remove(medianNode);
+				ArrayList<Node> rightNodes = new ArrayList<Node>();
+				ArrayList<Node> leftNodes = new ArrayList<Node>();
+				double relevantCoord = medianNode.getCoord(dimension);
+				
+				for(Node n: nodes)
+				{
+					if(n.getCoord(dimension) < relevantCoord)
+					{
+						leftNodes.add(n);
+					}
+					else if(n.getCoord(dimension) > relevantCoord)
+					{
+						rightNodes.add(n);
+					}
+				}
+				result.left = expand(leftNodes, depth+1);
+				result.right = expand(rightNodes, depth+1);
+				return result;
+			}
+			else if (nodes.size() == 2)
+			{
+				if (nodes.get(0).getCoord(depth%k) > nodes.get(1).getCoord(depth%k))
+				{
+					KDNode result = new KDNode(nodes.get(0));
+					result.left = new KDNode(nodes.get(1));
+					return result;
+					
+				}
+				else
+				{
+					KDNode result = new KDNode(nodes.get(1));
+					result.left = new KDNode(nodes.get(0));
+					return result;
+				}
+
+			}
+			else
+			{
+				return new KDNode(nodes.get(0));
+			}
+		}
 		
 	}
 }
