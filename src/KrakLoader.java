4c4eb31f482963a7a754a485a288bd74edec5a33
Lars Yndal Sorensen <yndal84@gmail.com>
2012-03-26 16:50:59 +0200

    Minor changes

--- src/KrakLoader.java
+++ src/KrakLoader.java
@@ -3,6 +3,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 
