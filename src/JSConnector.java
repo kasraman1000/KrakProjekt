fb56888cbfd9873f00f9d513bdd212838306dcdc
Filip Hjermind <filiphjermind@gmail.com>
2012-03-28 10:34:51 +0200

    small changes

--- src/JSConnector.java
+++ src/JSConnector.java
@@ -17,11 +17,10 @@ public class JSConnector {
 			ServerSocket ss = new ServerSocket(80);
 			Socket s = ss.accept();
 			BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
-//			System.out.println("input: " + input.readLine());
 			readParameters(input);
 			DataOutputStream output = new DataOutputStream(s.getOutputStream());
 			output.writeUTF("<g><line x1=\"400\" y1=\"200\" x2=\"0\" y2=\"0\" style=\"stroke:black;stroke-width:5\" /><line x1=\"400\" y1=\"200\" x2=\"0\" y2=\"400\" style=\"stroke:yellow;stroke-width:5\"/></g>");
-			output.flush();
+//			output.flush();
 			output.close();
 			s.close();
 			ss.close();
