ef1d2e16c6fab787c4e840e8b30c915a278eebfc
Filip Hjermind <filiphjermind@gmail.com>
2012-04-03 12:46:46 +0200

    very small changes

--- src/JSConnector.java
+++ src/JSConnector.java
@@ -17,7 +17,7 @@ public class JSConnector {
 	public JSConnector(Controller c) {
 		controller = c;
 		try {
-			//the parameter in ServerSocket is 80 because that is the default port for localhost
+			//the parameter in ServerSocket is 80 because it is the default port for localhost
 			ServerSocket ss = new ServerSocket(80);
 			listenForBrowserRequest(ss);
 		} catch (IOException e) {
