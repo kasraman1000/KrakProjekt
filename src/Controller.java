c61468893872d1ea6ea97c20b84a9f28ed2e26af
Lars Yndal Sorensen <yndal84@gmail.com>
2012-04-23 12:42:51 +0200

    .

--- src/Controller.java
+++ src/Controller.java
@@ -30,6 +30,8 @@ public class Controller {
 	
 	public static void main(String[] args) {
 		Controller controller = new Controller();
+		
+//		HashMap<Integer, HashMap<String,>>
 	}
 	
 	
--- src/routing/Routing.java
+++ src/routing/Routing.java
@@ -25,10 +25,10 @@ public class Routing {
 		
 		double start = System.nanoTime();
 		Routing ty = new Routing();
-		KrakLoader.load("kdv_unload.txt", "kdv_node_unload.txt");
-		EdgeWeightedDigraph graph = KrakLoader.getGraph();
+		Loader.load("kdv_unload.txt", "kdv_node_unload.txt");
+		EdgeWeightedDigraph graph = Loader.getGraph();
 		double end = System.nanoTime();
-		System.out.println("Time taken to create graph: " + (end-start)/1000000000 + " seconds");
+		System.out.println("Time taken to load: " + (end-start)/1000000000 + " seconds");
 		
 		double dijkstraStart = System.nanoTime();
 		DijkstraSP dij = new DijkstraSP(graph);
@@ -50,95 +50,95 @@ public class Routing {
 	}
 	
 	
-	/**
-	 * This class may most likely be edited to cooperate with the KrakLoader in some way.
-	 */
-	public EdgeWeightedDigraph loadGraph(String edgeFilePath, String nodeFilePath){
-		In inEdges = new In(new File(edgeFilePath));
-		In inNodes = new In(new File(nodeFilePath));
-		//Skip first line
-		inEdges.readLine();
-		inNodes.readLine();
-		
-		
-		
-		HashMap<Integer,Point> coordArray = new HashMap<Integer,Point>();
-		String[] nodeArray;
-		int nodeId;
-		double xCoord;
-		double yCoord;
-		Point point = new Point();
-		while(inNodes.hasNextLine()){
-			nodeArray = inNodes.readLine().split(",");
-			
-			/**
-			 * OBS Læg mærke til at ID'et bliver minuset med 1!!!!
-			 */
-			nodeId = Integer.valueOf(nodeArray[2])-1;
-			xCoord = Double.valueOf(nodeArray[3]);
-			yCoord = Double.valueOf(nodeArray[4]);
-			point.setLocation(xCoord, yCoord);
-			coordArray.put(nodeId, point);
-		}
-	
-				
-		ArrayList<DirectedEdge> edges = new ArrayList<>();
- 
-		EdgeWeightedDigraph graph;
-		Set<Integer> nodes = new HashSet<Integer>();
-		String[] tempStringArr;
-		int from;
-		int to;
-		double dist;
-		double time;
-		Point fromPoint = new Point();
-		Point toPoint = new Point();
-		String direction;
-		while(inEdges.hasNextLine()){
-			tempStringArr = inEdges.readLine().split(",");
-			
-			
-			/**
-			 * OBS Læg mærke til at ID'erne bliver minuset med 1!!!!
-			 */
-			from = Integer.valueOf(tempStringArr[0])-1;
-			to = Integer.valueOf(tempStringArr[1])-1;
-			dist = Double.valueOf(tempStringArr[2]);
-			
-			//time in minuttes
-			time = Double.valueOf(tempStringArr[26]);
-			
-			//tf = to->from
-			//ft = from->to
-			//n = no driving allowed
-			//<blank> = no restrictions
-			direction = tempStringArr[27]; 
-			
-			fromPoint.setLocation(coordArray.get(from).getX(), coordArray.get(from).getY());
-			toPoint.setLocation(coordArray.get(to).getX(), coordArray.get(to).getY());
-			
-			if      (direction.equals("'tf'")) edges.add(new DirectedEdge(from, to, dist, time, fromPoint, toPoint));
-			else if (direction.equals("'ft'")) edges.add(new DirectedEdge(to, from, dist, time, fromPoint, toPoint));
-			else if (!direction.equals("'n'")) {
-				edges.add(new DirectedEdge(from, to, dist, time, fromPoint, toPoint));
-				edges.add(new DirectedEdge(to, from, dist, time, fromPoint, toPoint));
-			}
-			nodes.add(to);
-			nodes.add(from);
-		}
-		
-		graph = new EdgeWeightedDigraph(nodes.size());
-		System.out.println("Edges.size(): " + edges.size());
-		
-		for(DirectedEdge e : edges){
-			graph.addEdge(e);
-		}
-		
-		System.out.println("Graph:");
-		System.out.println(graph.E() + " edges and " + graph.V() + " nodes");
-		
-		return graph;
-	}
+//	/**
+//	 * This class may most likely be edited to cooperate with the KrakLoader in some way.
+//	 */
+//	public EdgeWeightedDigraph loadGraph(String edgeFilePath, String nodeFilePath){
+//		In inEdges = new In(new File(edgeFilePath));
+//		In inNodes = new In(new File(nodeFilePath));
+//		//Skip first line
+//		inEdges.readLine();
+//		inNodes.readLine();
+//		
+//		
+//		
+//		HashMap<Integer,Point> coordArray = new HashMap<Integer,Point>();
+//		String[] nodeArray;
+//		int nodeId;
+//		double xCoord;
+//		double yCoord;
+//		Point point = new Point();
+//		while(inNodes.hasNextLine()){
+//			nodeArray = inNodes.readLine().split(",");
+//			
+//			/**
+//			 * OBS Læg mærke til at ID'et bliver minuset med 1!!!!
+//			 */
+//			nodeId = Integer.valueOf(nodeArray[2])-1;
+//			xCoord = Double.valueOf(nodeArray[3]);
+//			yCoord = Double.valueOf(nodeArray[4]);
+//			point.setLocation(xCoord, yCoord);
+//			coordArray.put(nodeId, point);
+//		}
+//	
+//				
+//		ArrayList<DirectedEdge> edges = new ArrayList<>();
+// 
+//		EdgeWeightedDigraph graph;
+//		Set<Integer> nodes = new HashSet<Integer>();
+//		String[] tempStringArr;
+//		int from;
+//		int to;
+//		double dist;
+//		double time;
+//		Point fromPoint = new Point();
+//		Point toPoint = new Point();
+//		String direction;
+//		while(inEdges.hasNextLine()){
+//			tempStringArr = inEdges.readLine().split(",");
+//			
+//			
+//			/**
+//			 * OBS Læg mærke til at ID'erne bliver minuset med 1!!!!
+//			 */
+//			from = Integer.valueOf(tempStringArr[0])-1;
+//			to = Integer.valueOf(tempStringArr[1])-1;
+//			dist = Double.valueOf(tempStringArr[2]);
+//			
+//			//time in minuttes
+//			time = Double.valueOf(tempStringArr[26]);
+//			
+//			//tf = to->from
+//			//ft = from->to
+//			//n = no driving allowed
+//			//<blank> = no restrictions
+//			direction = tempStringArr[27]; 
+//			
+//			fromPoint.setLocation(coordArray.get(from).getX(), coordArray.get(from).getY());
+//			toPoint.setLocation(coordArray.get(to).getX(), coordArray.get(to).getY());
+//			
+//			if      (direction.equals("'tf'")) edges.add(new DirectedEdge(from, to, dist, time, fromPoint, toPoint));
+//			else if (direction.equals("'ft'")) edges.add(new DirectedEdge(to, from, dist, time, fromPoint, toPoint));
+//			else if (!direction.equals("'n'")) {
+//				edges.add(new DirectedEdge(from, to, dist, time, fromPoint, toPoint));
+//				edges.add(new DirectedEdge(to, from, dist, time, fromPoint, toPoint));
+//			}
+//			nodes.add(to);
+//			nodes.add(from);
+//		}
+//		
+//		graph = new EdgeWeightedDigraph(nodes.size());
+//		System.out.println("Edges.size(): " + edges.size());
+//		
+//		for(DirectedEdge e : edges){
+//			graph.addEdge(e);
+//		}
+//		
+//		System.out.println("Graph:");
+//		System.out.println(graph.E() + " edges and " + graph.V() + " nodes");
+//		
+//		return graph;
+//	}
 
 
 }
