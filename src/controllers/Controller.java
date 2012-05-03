b0de5edfb15123fb69eaee4dad4177b855254331
Mark Thorhauge <markthorh@gmail.com>
2012-05-03 15:20:02 +0200

    called initialze at roadselector

--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -29,7 +29,7 @@ public class Controller {
 		kdTree = KDTree.getTree();
 		try {
 			Loader.load("kdv_node_unload.txt","kdv_unload.txt", "zip_codes.txt");
-			kdTree.initialize(Loader.getNodesForKDTree());
+			RoadSelector.initialize(Loader.getNodesForKDTree());
 			EdgeParser.build(Loader.getEdgesForTranslator());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
