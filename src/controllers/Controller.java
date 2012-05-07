6f64d56e9788f6df194a9dadf03a83634b0d62bc
Lars Yndal Sorensen <yndal84@gmail.com>
2012-05-07 16:20:16 +0200

    Refactoring the whole routing thing - again!!

--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -6,10 +6,12 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 
+import errorHandling.ServerStartupException;
+import errorHandling.StatusCode;
+
 import models.*;
 import views.*;
-import routing.*;
-import errorHandling.*;
+
 /**
  * The main controller responsible for program flow
  */
@@ -25,12 +27,16 @@ public class Controller {
 		double start = System.nanoTime();
 		System.out.println("System startup - please wait...");
 		try {
+			/*
+			Loader.load("zealand_node.txt","zealand_edge.txt", "zip_codes.txt");
+			/*/
 			Loader.load("kdv_node_unload.txt","kdv_unload.txt", "zip_codes.txt");
+			//*/
 			RoadSelector.initialize(Loader.getNodesForKDTree());
-			EdgeParser.build(Loader.getEdgesForTranslator());
-		} 
-		catch (ServerStartupException e) {
-			ErrorHandler.handleServerStartupException(e);
+			routing.EdgeParser.build(Loader.getEdgesForTranslator());
+		} catch (ServerStartupException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		xml = new XML();
 		double end = System.nanoTime();
@@ -54,13 +60,9 @@ public class Controller {
 	 * @return XML String containing all the roads in the Region
 	 */
 	public static String getXmlString(Region region, double bufferPercent){
-		System.out.println("Controller.getXmlString() - the first region: " + region);
-		//Region newRegion = new Region(region.getLeftPoint()[0], region.getLeftPoint()[1], region.getRightPoint()[0], region.getRightPoint()[1]);
 		Road[] roads = RoadSelector.search(region, bufferPercent);
 		String s = "";
-//		Region newRegion = new Region(Road.getOrigo()[0], Road.getOrigo()[1], Road.getTop()[0], Road.getTop()[1]);
 		try {
-			System.out.println("Controller.getXmlString() - " + region);
 			s = xml.createString(roads, null, region, StatusCode.ALL_WORKING);
 		} catch (TransformerConfigurationException e) {
 			// TODO Auto-generated catch block
@@ -87,45 +89,9 @@ public class Controller {
 	public static String getRoadAndRoute(String fromAddress, String toAddress, boolean isLengthWeighted, double bufferPercent) {
 		double startTime = System.nanoTime();
 		
-		
-		String[] fromAddressArray = AddressParser.parseAddress(fromAddress);
-		String[] toAddressArray = AddressParser.parseAddress(toAddress);
-		PathPreface pathPrefaceFrom = null;
-		PathPreface pathPrefaceTo = null;
-		
-		try {
-			pathPrefaceFrom = EdgeParser.findPreface(fromAddressArray);
-			pathPrefaceTo = EdgeParser.findPreface(toAddressArray);
-		} catch (Exception e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
-		if(pathPrefaceFrom == null) System.err.println("Controller.getRoadAndRoute() - pathPrefaceFrom == null at line 108!!!!");
-		if(pathPrefaceTo == null) System.err.println("Controller.getRoadAndRoute() - pathPrefaceTo == null at line 109!!!!");
-		
-		
-		//Randomly chosen because of later tests of the exact id (performed in EdgesAndRoadsConverter.checkStartAndTargetOfDijkstra())
-		int firstNodeId = pathPrefaceFrom.getEdge1().to(); 
-		int lastNodeId = pathPrefaceTo.getEdge1().to(); 
-		
-		
-		//Load the graph into Dijkstra and find the path
-		DijkstraSP dij = new DijkstraSP(Loader.getGraph());
-		Stack<KrakEdge> routeEdges = dij.findRoute(firstNodeId,  lastNodeId, isLengthWeighted);
-
-		//Convert from stack to []
-		KrakEdge[] routeEdgesArray = EdgesAndRoadsConverter.convertRouteStackToArray(routeEdges);
-		
-		//Correct start and end of [] and do the house number thing 
-		Road[] route = null;
-		try {
-			route = EdgesAndRoadsConverter.checkStartAndTargetOfDijkstra(routeEdgesArray, pathPrefaceFrom, pathPrefaceTo);
-		} catch (Exception e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
+		RouteFinder routeFinder = new RouteFinder(Loader.getGraph());
+		Road[] route = routeFinder.getRoute(fromAddress, toAddress, isLengthWeighted);
+				
 		Region newRegion = new Region(Road.getOrigo()[0], Road.getOrigo()[1], Road.getTop()[0], Road.getTop()[1]);	
 		Road[] roads = RoadSelector.search(newRegion, bufferPercent);
 
@@ -143,7 +109,6 @@ public class Controller {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 		
 		double endTime = System.nanoTime();
 		System.out.println("Controller.getRoadAndRoute() - Time taken to get route and roads: " + (endTime-startTime)/1e9 + " seconds");
---
+++ src/models/RouteFinder.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package models;
+
+import routing.*;
+
+/**
+ * @author Yndal
+ *
+ */
+public class RouteFinder {
+	KrakEdgeWeightedDigraph graph;
+	
+	public RouteFinder(KrakEdgeWeightedDigraph graph){
+		this.graph = graph;
+	}
+	
+	public Road[] getRoute(String from, String to, boolean isLengthWeighted){
+		String[] fromAddressArray = AddressParser.parseAddress(from);
+		String[] toAddressArray = AddressParser.parseAddress(to);
+		
+		PathPreface pathPrefaceFrom = null;
+		PathPreface pathPrefaceTo = null;
+		try {
+			pathPrefaceFrom = EdgeParser.findPreface(fromAddressArray);
+			pathPrefaceTo = EdgeParser.findPreface(toAddressArray);
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		Road[] result = getRoute(pathPrefaceFrom, pathPrefaceTo, isLengthWeighted);
+		
+		return result;
+	}
+	
+	
+	public Road[] getRoute(PathPreface pathPrefaceFrom, PathPreface pathPrefaceTo, boolean isLengthWeighted){
+		//Randomly chosen because of later tests of the exact id (performed in EdgesAndRoadsConverter.checkStartAndTargetOfDijkstra())
+		int firstNodeId; 
+		int lastNodeId; 
+		
+		if(pathPrefaceFrom.getEdge1() != null) firstNodeId = pathPrefaceFrom.getEdge1().to();
+		else firstNodeId = pathPrefaceFrom.getEdge2().to();
+
+		if(pathPrefaceTo.getEdge1() != null) lastNodeId = pathPrefaceTo.getEdge1().to();
+		else lastNodeId = pathPrefaceFrom.getEdge2().to();
+
+		
+		//Load the graph into Dijkstra and find the path
+		DijkstraSP dij = new DijkstraSP(Loader.getGraph());
+		Stack<KrakEdge> routeEdges = dij.findRoute(firstNodeId,  lastNodeId, isLengthWeighted);
+
+		//Convert from stack to []
+		KrakEdge[] routeEdgesArray = EdgesAndRoadsConverter.convertRouteStackToArray(routeEdges);
+		
+		//Correct start and end of [] and do the house number thing 
+		Road[] route = null;
+		try {
+			route = EdgesAndRoadsConverter.checkStartAndTargetOfDijkstra(routeEdgesArray, pathPrefaceFrom, pathPrefaceTo);
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+				
+		
+		return route;
+	}
+}
